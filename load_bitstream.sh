e75335032974630e8dd89193df7d86a9553a98ee
Andrew Lukefahr <lukefahr@iu.edu>
2021-01-14 19:05:29 -0500

    Updates to load_bitstream.sh

--- load_bitstream.sh
+++ load_bitstream.sh
@@ -1,13 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash 
+
+AUTO_IP=192.168.2.99
 
 if [ "$#" -gt 0 ]; then
     IP=$1
 else
-    IP="192.168.2.99"
+    IP=$AUTO_IP
 fi
 
 echo "Pynq IP: [$IP]"
-
 
 PROJECT="P3_EMA"
 echo "Project: $PROJECT"
@@ -18,7 +19,12 @@ HWH=$(ls $(pwd)/src/vsrc/design_fpga/hw_handoff/*.hwh | awk '{print $NF}')
 PYNQ=xilinx@$IP
 PRIV_KEY=$(pwd)/.id_rsa.xilinx.priv
 
+
 ssh -i ${PRIV_KEY} ${PYNQ} "mkdir -p ~/jupyter_notebooks/${PROJECT}" 
+
+echo "Uploading Bitstream"
 scp -i ${PRIV_KEY} ${BIT} ${PYNQ}:~/jupyter_notebooks/P3_EMA/Pynq/bitstream.bit
+
+echo "Uploading Hardware Handoff File"
 scp -i ${PRIV_KEY} ${HWH} ${PYNQ}:~/jupyter_notebooks/P3_EMA/Pynq/bitstream.hwh
 
